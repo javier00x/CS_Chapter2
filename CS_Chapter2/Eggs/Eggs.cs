61a8c88f007c7ef2baeddde2f0ef987a02d33a83
javier00x <javierf7900@gmail.com>
2017-02-08 14:15:02 -0500

    did egg work

--- CS_Chapter2/Eggs/Eggs.cs
+++ CS_Chapter2/Eggs/Eggs.cs
@@ -9,13 +9,18 @@ namespace Eggs
     {
         static void Main(string[] args)
         {
-            int chincken = 4;
-            int Eggs = 127;
-            int Dozens = Eggs / 12;
-            int Eggremainder = Eggs % 12;
-            string name1 = "Dozen";
-            string name2 = "Egg";
-            
+            int chicken1 = 4;
+            int chicken2 = 6;
+            int chicken3 = 8;
+            int chicken4 = 3;
+            int totalEggs = (chicken1 + chicken2 + chicken3 + chicken4);
+            int dozen = totalEggs / 12;
+            int remainderEgg = (totalEggs % 12);
+            string name1 = "dozen";
+
+            Console.WriteLine("the amount of eggs for this mouth is {0}. in all we got {1} dozen and {2} eggs this mouth", totalEggs, dozen, remainderEgg);
+
+
 
         }
     }
