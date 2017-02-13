3ff79a9ff6e3958a9f203b40ef5370b58119c052
javier00x <javierf7900@gmail.com>
2017-02-13 13:50:56 -0500

    did egg interactive

--- CS_Chapter2/EggsInteractive/Program.cs
+++ CS_Chapter2/EggsInteractive/Program.cs
@@ -9,10 +9,14 @@ namespace EggsInteractive
     {
         static void Main(string[] args)
         {
-            int chicken1 = 4;
-            int chicken2 = 6;
-            int chicken3 = 8;
-            int chicken4 = 3;
+            Console.Write("Enter the number of eggs for chicken #1 : ");
+            int chicken1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number of eggs for chicken #2 : ");
+            int chicken2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number of eggs for chicken #3 : ");
+            int chicken3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number of eggs for chicken #4 : ");
+            int chicken4 = Convert.ToInt32(Console.ReadLine());
             int totalEggs = (chicken1 + chicken2 + chicken3 + chicken4);
             int dozen = totalEggs / 12;
             int remainderEgg = (totalEggs % 12);
